4e6bf41e6975731ecdfdc7db30e9206936f14456
rueloparente <rueloparentedev@gmail.com>
2024-07-24 12:37:49 +0100

    Add RemainderOperator

---
+++ src/main/java/com/rueloparente/bloco1/RemainderOperator.java
@@ -0,0 +1,32 @@
+package com.rueloparente.bloco1;
+
+public class RemainderOperator {
+
+
+    public static int digitsOfRealNumber (int num){
+        if (num == 0) return 1;
+
+        num = Math.abs(num);
+        return (int) (Math.log10(num) + 1);
+    }
+
+    public static int[] algorithmsOfNumberIntoArray (int num){
+        int numberDigits = digitsOfRealNumber(num);
+        int[] digitsInArray = new int[numberDigits];
+
+        for (int i = 0; i < numberDigits; i++){
+            digitsInArray[i] = Math.abs(num % 10);
+            num = num/10;
+        }
+        return digitsInArray;
+    }
+
+    public static int numberEvenDigitsInNumber (int num){
+        int[] digits =  algorithmsOfNumberIntoArray(num);
+        int counter = 0;
+        for (int i = 0; i<digits.length; i++){
+            if (EvenOrOdd.checksIfNumberIsPair(digits[i])) counter ++;
+        }
+        return counter;
+    }
+}
---
+++ src/test/java/com/rueloparente/bloco1/RemainderOperatorTest.java
@@ -0,0 +1,131 @@
+package com.rueloparente.bloco1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RemainderOperatorTest {
+    @Test
+    void shouldReturnNumberOfDigitsInNegativeInteger() {
+        // Arrange
+        int num = -123;
+        int expected = 3;
+        // Act
+        int actual = RemainderOperator.digitsOfRealNumber(num);
+        // Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnNumberOfDigitsInNegativeSingleDigitInteger() {
+        // Arrange
+        int num = -5;
+        int expected = 1;
+        // Act
+        int actual = RemainderOperator.digitsOfRealNumber(num);
+        // Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnNumberOfDigitsInZero() {
+        // Arrange
+        int num = 0;
+        int expected = 1;
+        // Act
+        int actual = RemainderOperator.digitsOfRealNumber(num);
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnNumberOfDigitsInInteger() {
+        // Arrange
+        int num = 123;
+        int expected = 3;
+        // Act
+        int actual = RemainderOperator.digitsOfRealNumber(num);
+        // Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnDigitsOfNumberInArray() {
+        // Arrange
+        int num = 123;
+        int[] expected = {3, 2, 1};
+        // Act
+        int[] actual = RemainderOperator.algorithmsOfNumberIntoArray(num);
+        // Assert
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnDigitsOfNegativeNumberInArray() {
+        // Arrange
+        int num = -123;
+        int[] expected = {3, 2, 1};
+        // Act
+        int[] actual = RemainderOperator.algorithmsOfNumberIntoArray(num);
+        // Assert
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnDigitsOfSingleDigitNumberInArray() {
+        // Arrange
+        int num = 5;
+        int[] expected = {5};
+        // Act
+        int[] actual = RemainderOperator.algorithmsOfNumberIntoArray(num);
+        // Assert
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnDigitsOfZeroInArray() {
+        // Arrange
+        int num = 0;
+        int[] expected = {0};
+        // Act
+        int[] actual = RemainderOperator.algorithmsOfNumberIntoArray(num);
+        // Assert
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnNumberOfEvenDigitsInNumber() {
+        // Arrange
+        int num = 123;
+        int expected = 1;
+        // Act
+        int actual = RemainderOperator.numberEvenDigitsInNumber(num);
+        // Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnNumberOfEvenDigitsInNegativeNumber() {
+        // Arrange
+        int num = -123;
+        int expected = 1;
+        // Act
+        int actual = RemainderOperator.numberEvenDigitsInNumber(num);
+        // Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnNumberOfEvenDigitsInSingleDigitNumber() {
+        // Arrange
+        int num = 5;
+        int expected = 0;
+        // Act
+        int actual = RemainderOperator.numberEvenDigitsInNumber(num);
+        // Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnNumberOfEvenDigitsInZero() {
+        // Arrange
+        int num = 0;
+        int expected = 1;
+        // Act
+        int actual = RemainderOperator.numberEvenDigitsInNumber(num);
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+}
