5cbe2567e97ac675e162f59f07a0368a1f59de22
rueloparente <rueloparentedev@gmail.com>
2024-07-29 18:26:03 +0100

    Add Armstrong class + method to check Armstrong numbers and range exercises

--- src/main/java/com/rueloparente/bloco1/Armstrong.java
+++ src/main/java/com/rueloparente/bloco1/Armstrong.java
@@ -1,5 +1,7 @@
 package com.rueloparente.bloco1;
 
+import static java.lang.Double.NaN;
+
 public class Armstrong {
     public static boolean verifyIfNumberIsArmstrong (int num) {
         num = Math.abs(num);
@@ -12,5 +14,19 @@ public class Armstrong {
             num /= 10;
         }
         return originalNumber == resultingNumber;
+    }
+
+    public static double firstArmstrongNumberInGivenRange (int min, int max){
+        for (int i = min; i <= max; i++){
+            if (verifyIfNumberIsArmstrong(i)) return i;
+        }
+        return NaN;
+    }
+
+    public static double highestArmstrongNumberInGivenRange (int min, int max){
+        for (int i=max; i >= min; i--){
+            if (verifyIfNumberIsArmstrong(i)) return i;
+        }
+        return NaN;
     }
 }
--- src/test/java/com/rueloparente/bloco1/ArmstrongTest.java
+++ src/test/java/com/rueloparente/bloco1/ArmstrongTest.java
@@ -56,4 +56,71 @@ class ArmstrongTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    void shouldReturnFirstArmstrongNumberInGivenRange() {
+        // Arrange
+        int min = 2;
+        int max = 1000;
+        double expected = 153;
+        // Act
+        double actual = Armstrong.firstArmstrongNumberInGivenRange(min, max);
+        // Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnFirstArmstrongNumberInGivenRangeIfRangeIsSingleDigit() {
+        // Arrange
+        int min = 1;
+        int max = 9;
+        double expected = 1;
+        // Act
+        double actual = Armstrong.firstArmstrongNumberInGivenRange(min, max);
+        // Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnFirstArmstrongNumberWhenAtEndOfRange() {
+        // Arrange
+        int min = 2;
+        int max = 153;
+        double expected = 153;
+        // Act
+        double actual = Armstrong.firstArmstrongNumberInGivenRange(min, max);
+        // Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnNaNIfNoArmstrongNumberInGivenRange() {
+        // Arrange
+        int min = 2;
+        int max = 152;
+        double expected = Double.NaN;
+        // Act
+        double actual = Armstrong.firstArmstrongNumberInGivenRange(min, max);
+        // Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldReturnHighestArmstrongNumberInGivenRange() {
+        // Arrange
+        int min = 1;
+        int max = 1000;
+        double expected = 407;
+        // Act
+        double actual = Armstrong.highestArmstrongNumberInGivenRange(min, max);
+        // Assert
+        assertEquals(expected, actual);
+    }
+    @Test
+    void shouldReturnNaNIfNoArmstrongNumberInGivenRangeForHighest() {
+        // Arrange
+        int min = 2;
+        int max = 152;
+        double expected = Double.NaN;
+        // Act
+        double actual = Armstrong.highestArmstrongNumberInGivenRange(min, max);
+        // Assert
+        assertEquals(expected, actual);
+    }
 }
